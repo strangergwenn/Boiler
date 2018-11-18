15bf30a96ad5f2bd3982a0f794469e142b9c26cc
Gwennaël Arbona <gwennael.arbona@gmail.com>
2018-11-18 12:19:12 +0100

    Use UE4 cooking structure for the mod output

--- sources/main.cpp
+++ sources/main.cpp
@@ -25,46 +25,54 @@ int main(int argc, char** argv)
 		return EXIT_SUCCESS;
 	}
 
-	// Build paths
-	std::string ModsDirectory = "Mods";
-	std::string modDirectory = ModsDirectory + "/" + modName;
-	std::string pluginFileName = modDirectory + "/" + modName + ".uplugin";
+	// Mod paths
+	std::string modsDirectory = "Mods";
+	std::string modDirectory = modsDirectory + "/" + modName;
+	std::string modFileName = modDirectory + "/" + modName + ".uplugin";
 	std::string modPreviewImage = modDirectory + "/Preview.png";
+
+	// Cooked pak file paths
 	std::string modContentDirectoryWindows = modDirectory + "/Saved/StagedBuilds/WindowsNoEditor/" + gameName + "/Mods/" + modName + "/Content/Paks/WindowsNoEditor/";
 	std::string modContentDirectoryLinux = modDirectory + "/Saved/StagedBuilds/LinuxNoEditor/" + gameName + "/Mods/" + modName + "/Content/Paks/LinuxNoEditor/";
 	std::string modContentDirectoryMac = modDirectory + "/Saved/StagedBuilds/MacNoEditor/" + gameName + "/Mods/" + modName + "/Content/Paks/MacNoEditor/";
-	std::string modContentBuildDirectory = modDirectory + "/Build";
 
-	// Copy Windows & Linux folders to a unified folder
+	// Destination paths
+	std::string modBuildDirectory = modDirectory + "/Build";
+	std::string modBuildContentDirectory = modBuildDirectory + "/Content";
+	std::string modBuildPakDirectory = modBuildContentDirectory + "/Paks";
+
 	try
 	{
 		// Create build folder
-		if (std::filesystem::exists(modContentBuildDirectory))
+		if (std::filesystem::exists(modBuildDirectory))
 		{
-			std::filesystem::remove_all(modContentBuildDirectory);
+			std::filesystem::remove_all(modBuildDirectory);
 		}
-		std::filesystem::create_directory(modContentBuildDirectory);
+		std::filesystem::create_directory(modBuildDirectory);
+		std::filesystem::create_directory(modBuildContentDirectory);
+		std::filesystem::create_directory(modBuildPakDirectory);
 
-		// Copy Windows
-		std::filesystem::copy(modContentDirectoryWindows, modContentBuildDirectory + "/Windows");
+		// Copy required files
+		std::filesystem::copy(modFileName, modBuildDirectory);
+		std::filesystem::copy(modContentDirectoryWindows, modBuildPakDirectory + "/WindowsNoEditor");
 
 		// Copy Linux
 		if (std::filesystem::exists(modContentDirectoryLinux))
 		{
-			std::filesystem::copy(modContentDirectoryLinux, modContentBuildDirectory + "/Linux");
+			std::filesystem::copy(modContentDirectoryLinux, modBuildPakDirectory + "/LinuxNoEditor");
 		}
 
 		// Copy Mac
 		if (std::filesystem::exists(modContentDirectoryMac))
 		{
-			std::filesystem::copy(modContentDirectoryMac, modContentBuildDirectory + "/Mac");
+			std::filesystem::copy(modContentDirectoryMac, modBuildPakDirectory + "/MacNoEditor");
 		}
 	}
 	catch (const std::filesystem::filesystem_error & ex)
 	{
 		std::cout << ex.what() << std::endl;
 		std::cout << "Required mod files could not be found !" << std::endl;
-		std::cout << " - Make sure " << pluginFileName << " exists." << std::endl;
+		std::cout << " - Make sure " << modFileName << " exists." << std::endl;
 		std::cout << " - Make sure " << modPreviewImage << " exists." << std::endl;
 		std::cout << " - Make sure " << modContentDirectoryWindows << " exists." << std::endl;
 
@@ -72,19 +80,19 @@ int main(int argc, char** argv)
 	}
 
 	// Print paths
-	std::cout << "Plugin file : " << pluginFileName << std::endl;
-	std::cout << "Content dir : " << modContentBuildDirectory << std::endl;
+	std::cout << "Plugin file : " << modFileName << std::endl;
+	std::cout << "Content dir : " << modBuildDirectory << std::endl;
 	std::cout << "Preview dir : " << modPreviewImage << std::endl;
 
 	// Read mod file
-	std::ifstream modFile(pluginFileName);
+	std::ifstream modFile(modFileName);
 	std::stringstream modFileContent;
 	modFileContent << modFile.rdbuf();
 
 	// Check result
 	if (modFileContent.str().length() == 0)
 	{
-		std::cout << "Plugin file " << pluginFileName << " could not be found." << std::endl;
+		std::cout << "Plugin file " << modFileName << " could not be found." << std::endl;
 		return EXIT_SUCCESS;
 	}
 
@@ -100,7 +108,7 @@ int main(int argc, char** argv)
 		tool.upload(
 			plugin["FriendlyName"].asString(),
 			plugin["Description"].asString(),
-			modContentBuildDirectory,
+			modBuildDirectory,
 			modPreviewImage);
 
 		tool.shutdown();
